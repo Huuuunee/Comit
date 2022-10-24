0b53a85f00f6414a730f24f849098f33f182ba3e
Huuuunee <s21024@gsm.hs.kr>
2022-10-24 23:38:42 +0900

    :sparkles: ErrorCode 추가

--- src/main/java/com/example/comit/global/exception/ErrorCode.java
+++ src/main/java/com/example/comit/global/exception/ErrorCode.java
@@ -6,6 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+
+    TOKEN_EXPIRATION("토큰이 만료 되었습니다.", 401),
+    TOKEN_NOT_VALID("토큰이 유효 하지 않습니다.", 401),
+    USER_ALREADY_EXIST("이미 존재하는 유저입니다",409),
     UNKNOWN("알 수 없는 에러",500);
 
     private final String message;
