8912bcfa8689053e51757d2b5d74b90ba0e9027c
Huuuunee <s21024@gsm.hs.kr>
2022-10-24 23:46:22 +0900

    :sparkles: security 구현

---
+++ src/main/java/com/example/comit/global/filter/JwtRequestFilter.java
@@ -0,0 +1,43 @@
+package com.example.comit.global.filter;
+
+import com.example.comit.global.exception.exceptionCollection.TokenNotValidException;
+import com.example.comit.global.security.jwt.TokenProvider;
+import com.example.comit.global.security.jwt.properties.JwtProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtProperties jwtProperties;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = request.getHeader("Authorization");
+        if(accessToken != null) {
+            tokenProvider.extractAllClaims(accessToken, jwtProperties.getAccessSecret());
+            System.out.print(tokenProvider.getTokenType(accessToken,jwtProperties.getAccessSecret()));
+            if (!tokenProvider.getTokenType(accessToken, jwtProperties.getAccessSecret()).equals("ACCESS_TOKEN"))
+                throw new TokenNotValidException("Token is not valid");
+            String email = tokenProvider.getUserId(accessToken, jwtProperties.getAccessSecret());
+            registerSecurityContext(request, email);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void registerSecurityContext(HttpServletRequest request, String email) {
+        UsernamePasswordAuthenticationToken authenticationToken = tokenProvider.authentication(email);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+}
---
+++ src/main/java/com/example/comit/global/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.example.comit.global.security;
+
+import com.example.comit.global.filter.JwtRequestFilter;
+import com.example.comit.global.security.handler.CustomAccessDeniedHandler;
+import com.example.comit.global.security.handler.CustomAuthenticationEntryPointHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sound.midi.Patch;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/business-cards").permitAll()
+                .antMatchers(HttpMethod.POST,
+                        "/auth/signup",
+                        "/auth",
+                        "/user",
+                        "/email"
+                ).permitAll()
+                .antMatchers(HttpMethod.PATCH, "/auth").permitAll()
+                .antMatchers(HttpMethod.HEAD,
+                        "/email"
+                ).permitAll()
+                .antMatchers(HttpMethod.PATCH,"/user").authenticated()
+                .anyRequest().authenticated();
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .exceptionHandling()
+                .accessDeniedHandler(new CustomAccessDeniedHandler())
+                .authenticationEntryPoint(new CustomAuthenticationEntryPointHandler());
+        http
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
