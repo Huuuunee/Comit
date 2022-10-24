61ff4e284923c51096a9ceee8fa087d66f84e406
Huuuunee <s21024@gsm.hs.kr>
2022-10-24 23:39:23 +0900

    :sparkles: config옵션 추가

--- src/main/java/com/example/comit/ComitApplication.java
+++ src/main/java/com/example/comit/ComitApplication.java
@@ -2,8 +2,14 @@ package com.example.comit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableCaching
 public class ComitApplication {
 
 	public static void main(String[] args) {
