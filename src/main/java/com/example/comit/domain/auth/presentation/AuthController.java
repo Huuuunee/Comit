74e1e1de6fc280234f371e87e6710ff77e58b4b5
Huuuunee <s21024@gsm.hs.kr>
2022-10-25 11:40:21 +0900

    :sparkles: 컨트롤러 post 매핑

--- src/main/java/com/example/comit/domain/auth/presentation/AuthController.java
+++ src/main/java/com/example/comit/domain/auth/presentation/AuthController.java
@@ -1,6 +1,9 @@
 package com.example.comit.domain.auth.presentation;
 
-import com.example.comit.domain.auth.presentation.dto.request.UserSignUpRequest;
+import com.example.comit.domain.auth.presentation.dto.request.SignInRequest;
+import com.example.comit.domain.auth.presentation.dto.request.SignUpRequest;
+import com.example.comit.domain.auth.presentation.dto.response.SignInResponse;
+import com.example.comit.domain.auth.services.SignInService;
 import com.example.comit.domain.auth.services.SignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,11 +21,18 @@ import javax.validation.Valid;
 public class AuthController {
 
     private final SignUpService signUpService;
+    private final SignInService signInService;
 
     @PostMapping("signup")
-    public ResponseEntity<Void> signUp(@RequestBody @Valid UserSignUpRequest userSignUpRequest){
+    public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpRequest userSignUpRequest){
         signUpService.execute(userSignUpRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping
+    public ResponseEntity<SignInResponse> signIn(@RequestBody @Valid SignInRequest signInRequest){
+        SignInResponse signInResponse = signInService.execute(signInRequest);
+        return new ResponseEntity<>(signInResponse,HttpStatus.OK);
     }
 
 }
